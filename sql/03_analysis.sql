560e9a19399193d17dabba1fea723332e669bd2b
Your Name Christian <you@example.comchristiannugal@gmai.com>
2026-03-25 15:03:46 +1100

    updated analysis sql file

--- sql/03_analysis.sql
+++ sql/03_analysis.sql
@@ -22,9 +22,10 @@ SELECT SUM(Total_Charges) AS revenue_lost
 FROM customers
 WHERE churn_flag = 1;
 
--- Calculating churn by contract: obtaining sum of customers and their churn rates
+-- Calculating churn and average revenue by contract: obtaining sum of customers and their churn rates & monthy charges
 SELECT CONTRACT,
 	COUNT(*) AS customers,
+    AVG(Monthly_Charges) AS avg_revenue,
     AVG(churn_flag) AS churn_rate
 FROM customers
 GROUP BY Contract;
@@ -40,6 +41,43 @@ SELECT
 FROM customers
 GROUP BY tenure_group;
 
+-- Measuring CLTV against churn
+SELECT churn_flag,
+	AVG(CLTV) AS avg_cltv
+FROM customers
+GROUP BY churn_flag;
+
+-- creating a feature impact visualisation consisting of Internet_Service, Tech_Support, Online_Security, 'Contract' (different from kpi query earlier), 'Payment_Method'
+SELECT 'Internet_Service' AS feature, Internet_Service AS category, ROUND(AVG(churn_flag), 3) AS churn_rate
+FROM compact_churn_analysis
+GROUP BY Internet_Service
+
+UNION ALL
+
+SELECT 'Tech_Support', Tech_Support, AVG(churn_flag)
+FROM compact_churn_analysis
+GROUP BY Tech_Support
+
+UNION ALL
+
+SELECT 'Online_Security', Online_Security, AVG(churn_flag)
+FROM compact_churn_analysis
+GROUP BY Online_Security
+
+UNION ALL
+
+SELECT 'Contract', Contract, AVG(churn_flag)
+FROM compact_churn_analysis
+GROUP BY Contract
+
+UNION ALL
+
+SELECT 'Payment_Method', Payment_Method, AVG(churn_flag)
+FROM compact_churn_analysis
+GROUP BY Payment_Method;
+
+-- DROP TABLE compact_churn_analysis;
+
 CREATE TABLE compact_churn_analysis AS
 SELECT
 	CustomerID,
@@ -47,6 +85,8 @@ SELECT
     Monthly_Charges,
     Total_Charges,
     Contract,
+    Tech_Support,
+    Online_Security,
     Internet_Service,
     Payment_Method,
     churn_flag
